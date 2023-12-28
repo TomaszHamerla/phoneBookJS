84d86415a6dabe788254223111e3d1559787cfc2
Tomasz Hamerla <tomaszhamerla@interia.pl>
2023-12-28 10:48:40 +0100

    refactored number and name valid

--- file.js
+++ file.js
@@ -1,16 +1,25 @@
-// projekt książka telefoniczna:
-// utwórz aplikację która będzie przechowywać kontakty w pamięci
-// aplikacja ma mieć możliwość dodawania nowych kontaktów, usuwania kontaktów
-// wyświetlania wszystkich kontaktów
-// wyszukiwania kontaktów po nazwie lub numerze telefonu
-// przy dodawanu kontaktu sprawdź czy taki kontakt już istnieje - jeżeli tak to wyświetl odpowiedni komunikat
-// sprawdź czy numer telefonu ma poprawny format (dla uproszczenia: ma mieć 9 znaków)
-// sprawdź czy nazwa kontaktu ma poprawny format (dla uproszczenia: conajmniej 3 znaki)
-// nie modyfikuj pliku index.html
-
 let contacts = [];
 document.getElementById("add-contact").addEventListener("click", () => {
   const name = document.getElementById("name").value;
   const number = document.getElementById("phoneNumber").value;
   console.log(name + number);
+  addContact(name, number);
 });
+
+function addContact(name, number) {
+  if (name.length < 3) {
+    alert("Contact name must be atleast 3 charactes long");
+    return false;
+  }
+
+  if (number.length !== 9) {
+    alert("Phone number must be 9 charactes long");
+    return false;
+  }
+  if (contacts.some((c) => c.name === name || c.number === number)) {
+    alert("Contact with this name or phone number already exits");
+    return false;
+  }
+  contacts.push({ name, number });
+  displayContacts(contacts);
+}
